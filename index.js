bf04579a0340e4dbfba9d5c120e7ca63c0ce0c22
serkankocaman1 <kemirgen@serkans-MacBook-Air.local>
2023-10-15 23:09:16 +0300

    new repo

---
+++ index.js
@@ -0,0 +1,78 @@
+const container = document.querySelector(".container")
+const searchBar = document.querySelector(".search-bar input")
+const searchBtn = document.querySelector(".search-btn")
+let messageAlert
+let searchKeyWord = "blade"
+
+let myWatchListArray = JSON.parse(localStorage.getItem("watchList"))
+
+searchBtn.addEventListener("click", function(){
+    searchKeyWord = searchBar.value
+    console.log(searchKeyWord)
+    containerItems = ""
+    movieApp()
+})
+
+let containerItems = ""
+
+async function movieApp(){
+    const response = await fetch(`https://www.omdbapi.com/?apikey=fb6d3e56&s=${searchKeyWord}&plot=full`)
+    const data = await response.json()
+    const searchArray = data.Search
+    if(searchArray){
+        searchArray.forEach(async element => {
+            const response = await fetch(`https://www.omdbapi.com/?apikey=fb6d3e56&i=${element.imdbID}&plot=short`)
+            const movie = await response.json()
+            containerItems += `
+            <div class="item">
+            <img class="movie-poster" src="${movie.Poster}">
+            <h3 class="movie-title">${movie.Title}</h3>
+            <div class="imdb-container">
+                <i class="fa-solid fa-star" style="color: #fec654;"></i>
+                <p class="imdb-rating">${movie.imdbRating}</p>
+            </div>
+            <p class="movie-time">${movie.Runtime}</p>
+            <p class="movie-genre">${movie.Genre}</p>
+            <a class="watchList-container" data-imdb="${element.imdbID}">
+                <i class="fa-solid fa-plus"></i>
+                <p>Watchlist</p>
+            </a>
+            <p class="movie-description">${movie.Plot}</p>
+            <section class="message">
+            <div class="message-item" data-imdb="${element.imdbID}">
+                <p>This movie or serie already added your watch list</p>
+            </div>
+            </section>
+            </div>
+            `
+            container.innerHTML = containerItems 
+            messageAlert = document.querySelectorAll(".message")
+        });
+    }
+} 
+
+
+document.addEventListener("click", e => {
+    const myImdbID = e.target.parentElement.dataset.imdb
+    if(myWatchListArray.includes(myImdbID)){
+        messageAlert.forEach(messageID => 
+        {
+            if(messageID.children[0].getAttribute("data-imdb") == e.target.parentElement.dataset.imdb){
+                messageID.style.display = "flex"
+                messageID.style.visibility = "visible";
+                setTimeout(function(){
+                    messageID.style.visibility = "hidden";
+                    setTimeout(function(){
+                        messageID.style.display = "none"
+                    },1000)
+                },1000)
+                messageID.children[0].children[0].textContent = "This movie or serie already added your watch list"
+            }
+        })
+    }else if(myImdbID === undefined){
+    }else{
+        myWatchListArray.push(myImdbID)
+        localStorage.setItem("watchList", JSON.stringify(myWatchListArray))
+    }
+    const localWatchListArray = JSON.parse(localStorage.getItem("watchList"))
+})
---
+++ my-watch-list.js
@@ -0,0 +1,48 @@
+const container = document.querySelector(".container")
+
+const watchListArray = JSON.parse(localStorage.getItem("watchList"))
+
+containerItems = ""
+
+async function watchListAPI(element, index){
+    const response = await fetch(`https://www.omdbapi.com/?apikey=fb6d3e56&i=${watchListArray[index]}&plot=short`)
+    const movie = await response.json()
+        containerItems += `
+        <div class="item">
+        <img class="movie-poster" src="${movie.Poster}">
+        <h3 class="movie-title">${movie.Title}</h3>
+        <div class="imdb-container">
+            <i class="fa-solid fa-star" style="color: #fec654;"></i>
+            <p class="imdb-rating">${movie.imdbRating}</p>
+        </div>
+        <p class="movie-time">${movie.Runtime}</p>
+        <p class="movie-genre">${movie.Genre}</p>
+        <a class="watchList-container" data-imdb="${element}">
+            <i class="fa-solid fa-minus"></i>
+            <p>Remove</p>
+        </a>
+        <p class="movie-description">${movie.Plot}</p>
+        <section class="message">
+        <div class="message-item" data-imdb="${element.imdbID}">
+            <p>This movie or serie already added your watch list</p>
+        </div>
+        </section>
+        </div>
+        `
+        container.innerHTML = containerItems
+}
+
+watchListArray.forEach((element, index) => {
+    console.log(element, index)
+    const movies = watchListAPI(element, index)
+});
+
+document.addEventListener("click", e => {
+    if(e.target.parentElement.className === "watchList-container"){
+        const imdbID = e.target.parentElement.dataset.imdb
+        const removeArray = JSON.parse(localStorage.getItem("watchList"))
+        const newArray = removeArray.filter(item => item !== imdbID)
+        localStorage.setItem("watchList", JSON.stringify(newArray))
+        e.target.parentElement.parentElement.remove()
+    }
+})
